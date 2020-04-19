648b690648a99b773ea235029c3c216862e48a6b
Haythem Draoui <59057708+DHaythem@users.noreply.github.com>
2020-04-19 22:53:44 +0200

    Update PercolationStats.java

--- Algorithms - Part 1/Assignment 1/PercolationStats.java
+++ Algorithms - Part 1/Assignment 1/PercolationStats.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
@@ -7,91 +8,72 @@ import edu.princeton.cs.algs4.StdStats;
 */
 
 public class PercolationStats {
-    
-    private int N;
-    private int T;
-    private double[] thresholds;
-    
-    /*
-     Creates T number of N * N percolation grids and calculates the percent of the nodes that need to be open in order for the grid to percolate.
-     After the PercolationStats object has finished initialising, the mean, standard deviation, and lower and upper confidence bounds can be returned.
-    */
-    
-    public PercolationStats(int N, int T) {
-        if (N <= 0 || T <= 0) throw new java.lang.IllegalArgumentException("N and T have to be greater thna zero");
-        
-        this.N = N;
-        this.T = T;
-        thresholds = new double[T];
+    private int trialCount;
+    private double[] trialResults;
+
+
+    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("N and T must be <= 0");
+        }
+        int gridSize = n;
+        trialCount = trials;
+        trialResults = new double[trialCount];
         
         //Create T instances of new Percolation object of size N.
-        for (int i = 0; i < N; i++) {
-            Percolation p = new Percolation(N);
-            
-            //Keep count of how nodes we have opened.
-            int openCount = 0;
+        for (int trial = 0; trial < trialCount; trial++) {
+            Percolation percolation = new Percolation(gridSize);
             
             //Continue to open nodes until the grid percolates.
-            while (!p.percolate()) {
-                openRandomNode(p);
-                openCount++;
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(1, gridSize + 1);
+                int col = StdRandom.uniform(1, gridSize + 1);
+                percolation.open(row, col);
             }
+            int openSites = percolation.numberOfOpenSites();
             
             //Store the result in an array to run queries over it later.
-            thresholds[i] = (double) openCount / (N * N);
+            double result = (double) openSites / (gridSize * gridSize);
+            trialResults[trial] = result;
         }
     }
-    
-    //Private helper function that will open a random closed node from a given percolation grid.
-    private void openRandomNode(Percolation p) {
-        boolean openNode = true;
-        int row = 0;
-        int col = 0;
 
-        //Repeat until we randomly find a closed node.
-        while (openNode) {
-            //Generate a random index between 1 and N + 1 (1-based grid, remember)
-            row = StdRandom.uniform(1, N + 1);
-            col = StdRandom.uniform(1, N + 1);
 
-            openNode = p.isOpen(row, col);
-        }
-
-        //If we reach here then we know that p[row, col] is an open node
-        p.open(row, col);
-    }
-    
-    //Returns the mean percentage of nodes that have to be open in order for the grid to percolate.
+    // sample mean of percolation threshold
     public double mean() {
-        return StdStats.stddev(thresholds);
+        return StdStats.mean(trialResults);
     }
-    
-    //Returns the standard deviation from the percentage of nodes that have to be open in order for the grid to percolate.
+
+    // sample standard deviation of percolation threshold
     public double stddev() {
-        return StdStats.stddev(thresholds);
+        return StdStats.stddev(trialResults);
     }
-    
-    //Return the lower bounds from the 95% confidence interval.
+
+    // low  endpoint of 95% confidence interval
     public double confidenceLo() {
-        return mean() - (1.96 * stddev()) / Math.sqrt(T);
+        return mean() - ((1.96 * stddev()) / Math.sqrt(trialCount));
+
     }
-    
-    //Return the upper bounds from the 95% confidence interval.
+
+    // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return mean() + (1.96 * stddev()) / Math.sqrt(T);
+        return mean() + ((1.96 * stddev()) / Math.sqrt(trialCount));
     }
-    
-    //Tests the class, outputs in format specified by the assignment
+
+    // test client (described below)
     public static void main(String[] args) {
-        
-        int N = Integer.parseInt(args[0]);
-        int T = Integer.parseInt(args[1]);
-        
-        PercolationStats s = new PercolationStats(N, T);
-        
-        System.out.println("mean:\t\t\t\t = " + s.mean());
-        System.out.println("mean:\t\t\t\t = " + s.stddev());
-        System.out.println("95% confidence interval:\t = " + s.confidenceLo() + "," + s.confidenceHi());
+        int gridSize = 10;
+        int trialCount = 10;
+        if (args.length >= 2) {
+            gridSize = Integer.parseInt(args[0]);
+            trialCount = Integer.parseInt(args[1]);
+        }
+        PercolationStats ps = new PercolationStats(gridSize, trialCount);
+
+        String confidence = ps.confidenceLo() + ", " + ps.confidenceHi();
+        StdOut.println("mean                    = " + ps.mean());
+        StdOut.println("stddev                  = " + ps.stddev());
+        StdOut.println("95% confidence interval = " + confidence);
     }
-    
 }
